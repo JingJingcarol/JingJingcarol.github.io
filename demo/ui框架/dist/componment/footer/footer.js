4700e1a09e7a73d9da81a1100bd6cd388b12c6bf
carol <carol@caroldeMacBook-Pro.local>
2020-03-22 16:37:10 +0800

    remove ui框架

--- demo/ui框架/dist/componment/footer/footer.js
+++
@@ -1,7 +0,0 @@
-$(document).on('click','.footer li',function(){
-    const footer = $(this).parents('.footer');
-    $(this).addClass('active').siblings().removeClass('active');
-    footer.css({
-        '--atpos' : (4.5 + $(this).index()*100/footer.find('li').length) + '%'
-    })
-})
